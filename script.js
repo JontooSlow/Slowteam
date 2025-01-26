2d185d6753f94c238df4a70a9d515e278eb50da0
JontooSlow <aartemuk@yandex.ru>
2025-01-26 13:21:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 const SPREADSHEET_ID = '1Z6MkmyCU_xELc_riP_xeCRzXr4rNhTQ2pyjwQ0ZcGYk';
 const API_KEY = 'AIzaSyBmSkNijS0qEa9j8ZrvFItYggN_FgXe5jg';
-const RANGE = "'Slow ranking'!B6:D35"; // Измените на нужный диапазон
+const RANGES = [
+    "'Slow ranking'!B5:D35",  // Phys
+    "'Slow ranking'!E5:G35",  // Light
+    "'Slow ranking'!H5:J35",  // Dark
+    "'Slow ranking'!K5:M35",  // Fire
+    "'Slow ranking'!N5:P35",  // Ice
+    "'Slow ranking'!Q5:S35",  // Elec
+    "'Slow ranking'!T5:V35"   // Force
+];
 
-async function fetchData() {
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${RANGE}?key=${API_KEY}`;
-    
+async function fetchData(range) {
+    const url = https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${range}?key=${API_KEY};
+
     try {
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        if (data.values) {
-            populateTable(data.values);
-        } else {
-            console.error('No data found in the sheet');
-        }
+        return data.values || [];
     } catch (error) {
         console.error('Error fetching data:', error);
+        return [];
     }
 }
 
-function populateTable(data) {
-    const tableBody = document.getElementById('results-table').querySelector('tbody');
-    // Очистить таблицу перед добавлением новых данных
+function populateTable(tableId, data) {
+    const tableBody = document.getElementById(tableId).querySelector('tbody');
     tableBody.innerHTML = '';
     data.forEach(row => {
         const tr = document.createElement('tr');
@@ -36,4 +40,22 @@ function populateTable(data) {
     });
 }
 
-fetchData();
+RANGES.forEach((range, index) => {
+    fetchData(range).then(data => {
+        populateTable(table${index+1}, data);
+    });
+});
+
+function toggleTable(button) {
+    const table = button.nextElementSibling;
+    table.style.display = table.style.display === 'none' ? 'table' : 'none';
+}
+
+function filterTables() {
+    const filter = document.getElementById('filter').value.toLowerCase();
+    document.querySelectorAll('.results-table tbody tr').forEach(row => {
+        const nameCell = row.children[1]; // Индекс 1 для столбца Name
+        const name = nameCell.textContent.toLowerCase();
+        row.style.display = name.includes(filter) ? '' : 'none';
+    });
+}
