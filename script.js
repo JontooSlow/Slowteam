fa71010f9eba075db43ce63fb450c03c2f414693
JontooSlow <aartemuk@yandex.ru>
2025-03-16 19:25:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const RANGES = [
     "'Slow ranking'!N6:P36",  // Ice
     "'Slow ranking'!Q6:S36",  // Elec
     "'Slow ranking'!T6:V36"   // Force
+    "'Slow ranking'!W6:Y36"   // Total
 ];
 
 async function fetchData(range) {
@@ -55,7 +56,7 @@ function filterTables() {
     });
 }
 document.addEventListener("DOMContentLoaded", async function() {
-    const contentIds = ['table1', 'table2', 'table3', 'table4', 'table5', 'table6', 'table7'];
+    const contentIds = ['table1', 'table2', 'table3', 'table4', 'table5', 'table6', 'table7', 'table8'];
     
     for (let i = 0; i < RANGES.length; i++) {
         const data = await fetchData(RANGES[i]);
